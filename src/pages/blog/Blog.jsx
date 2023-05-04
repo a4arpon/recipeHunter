791123f01abc4493a60157bde75380dd1d11c44d
Mr Wayne <a4arpon@gmail.com>
2023-05-04 12:00:56 +0600

    Question added

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -3,22 +3,22 @@ const Blog = () => {
     {
       _id: 1,
       q: 'Tell us the differences between uncontrolled and controlled components.',
-      a: 'To validate React props using PropTypes, import the PropTypes library, define a propTypes object, and set the prop types for each prop in the object. Use the propTypes object to validate that the props passed to a component match their expected types and values.'
+      a: 'In React, components can manage its own state or have their state managed by a parent component. Uncontrolled components are easier to use but offer less control, while controlled components offer more control. Controlled components are often used for forms or interactive components that require complex state management.'
     },
     {
       _id: 2,
-      q: 'How to validate React props using PropTypes tell under 50 words ChatGPT',
-      a: 'To validate React props using PropTypes, import the PropTypes library, define a propTypes object, and set the prop types for each prop in the object. Use the propTypes object to validate that the props passed to a component match their expected types and values.'
+      q: 'How to validate React props using PropTypes?',
+      a: 'To validate React props using PropTypes, import the PropTypes library, define a propTypes object, and set the prop types for each prop in the object. Use the propTypes object to check if the props passed to a component match their expected types and values.'
     },
     {
       _id: 3,
       q: 'Tell us the difference between nodejs and express js.',
-      a: 'To validate React props using PropTypes, import the PropTypes library, define a propTypes object, and set the prop types for each prop in the object. Use the propTypes object to validate that the props passed to a component match their expected types and values.'
+      a: 'Node.js is a runtime environment for executing JavaScript code outside of a web browser. Express.js is a web application framework built on top of Node.js. In other words, Node.js is the foundation, and Express.js is a tool that makes it easier to build web applications with Node.js.'
     },
     {
       _id: 4,
-      q: ' What is a custom hook, and why will you create a custom hook?',
-      a: 'To validate React props using PropTypes, import the PropTypes library, define a propTypes object, and set the prop types for each prop in the object. Use the propTypes object to validate that the props passed to a component match their expected types and values.'
+      q: 'What is a custom hook, and why will you create a custom hook?',
+      a: 'A custom hook is a reusable function that can be used in React components. It is a way to share code between components without having to write it all over again.'
     }
   ]
   return (
